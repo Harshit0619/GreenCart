2a3f587a1ecf563c892844a8d2972f6fc062946b
Harshit <harshitobra19@gmail.com>
2025-04-26 14:34:34 +0530

    initial commit

---
+++ src/components/seller/SellerLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAppContext } from '../../context/AppContext'
+
+const Sellerlogin = () => {
+    const {isSeller, setIsSeller , navigate} = useAppContext();``
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Sellerlogin
